94b0ffc9343a47cd950d107e4b70c0d3f9045310
tofuadmiral <fuad@fuadali.com>
2022-05-11 11:43:32 -0700

    WS-001: Added signup page with no link

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import SignIn from './SignIn';
 
 function Copyright() {
   return (
@@ -20,14 +21,6 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <SignIn />
   );
 }
